43728644020e4c84b54f1d78c9b9d8ae1e6f11a8
Brice Aminou <brice.aminou@gmail.com>
2018-02-13 14:35:11 -0500

    generate song payload task added

---
+++ workflow/tools/generate_song_payloads.py
@@ -0,0 +1,60 @@
+
+from utils import get_task_dict, save_output_json
+
+from overture_song_payload import DonorPayload
+from overture_song_payload import ExperimentPayload
+from overture_song_payload import FilePayload
+from overture_song_payload import SpecimenPayload
+from overture_song_payload import SamplePayload
+from overture_song_payload import SongPayload
+
+import sys
+import os
+import hashlib
+
+task_dict = get_task_dict(sys.argv[1])
+cwd = os.getcwd()
+
+input_directory = task_dict.get('input_directory')
+normal_bam = task_dict.get('normal_bam')
+tumor_bams = task_dict.get('tumor_bams')
+experiment = task_dict.get('experiment')
+analysis = task_dict.get('analysis')
+
+def create_payload_json(bam, analysis, experiment, input_directory, output_file):
+    donor_payload = DonorPayload(donor_gender=bam.get('donor').get('gender'),donor_submitter_id=bam.get('donor').get('submitter_id'))
+    experiment_payload = ExperimentPayload(aligned=experiment.get('aligned'),library_strategy=experiment.get('library_strategy'),reference_genome=experiment.get('reference_genome'))
+
+    file_path = os.path.join(input_directory,bam.get('minibam').get('bam_file_name'))
+    minibam_payload = FilePayload(file_access=bam.get('minibam').get('access'),file_name=bam.get('minibam').get('bam_file_name'),md5sum=hashlib.md5(file_path),file_type='BAM',file_size=os.stat(file_path).st_size)
+
+    file_path = os.path.join(input_directory,normal_bam.get('minibam').get('bai_file_name'))
+    minibai_payload = FilePayload(file_access=normal_bam.get('minibam').get('access'),file_name=normal_bam.get('minibam').get('bai_file_name'),md5sum=hashlib.md5(file_path),file_type='BAI',file_size=os.stat(file_path).st_size)
+
+    specimen_payload = SpecimenPayload(specimen_class=bam.get('sample').get('specimen').get('specimen_class'),
+                                   specimen_type=bam.get('sample').get('specimen').get('type'),
+                                   specimen_submitter_id=bam.get('sample').get('specimen').get('submitter_id'))
+
+    sample_payload = SamplePayload(donor_payload=donor_payload, sample_submitter_id=bam.get('sample').get('submitter_id'),sample_type=bam.get('sample').get('sample_type'),
+                               specimen_payload=specimen_payload)
+
+    song_payload = SongPayload(analysis_id=analysis.get('id'), analysis_type=analysis.get('type'),experiment_payload=experiment_payload)
+    song_payload.add_file_payload(minibam_payload)
+    song_payload.add_file_payload(minibai_payload)
+    song_payload.add_sample_payload(sample_payload)
+    song_payload.add_info('isPcawg',True)
+    song_payload.to_json_file(output_file)
+
+
+payloads = []
+
+create_payload_json(normal_bam, analysis, experiment, input_directory, os.path.join(input_directory, 'normal_minibam.json'))
+payloads.append(os.path.join(input_directory, 'normal_minibam.json'))
+
+for i in range(0,len(tumor_bams)):
+    create_payload_json(tumor_bams[i],analysis, experiment, input_directory, os.path.join(input_directory, 'tumor_minibam_'+str(i)+'.json'))
+    payloads.append(os.path.join(input_directory, 'tumor_minibam_'+str(i)+'.json'))
+
+save_output_json({
+    'payloads': payloads
+})
--- workflow/tools/upload.py
+++ workflow/tools/upload.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import json
 import subprocess
 from utils import get_task_dict, save_output_json, get_md5
 import sys
+
 import shutil
 
 
@@ -16,9 +17,8 @@ payload_container = "quay.io/baminou/dckr_song_generate_payload"
 upload_container = "quay.io/baminou/dckr_song_upload"
 subprocess.check_output(['docker','pull',payload_container])
 
+payloads = task_dict.get('input').get('payloads')
 input_directory = task_dict.get('input').get('input_directory')
-donor = task_dict.get('input').get('donor')
-experiment = task_dict.get('input').get('experiment')
 normal_minibam_name = task_dict.get('input').get('normal_bam').get('minibam').get('bam_file_name')
 normal_minibai_name = task_dict.get('input').get('normal_bam').get('minibam').get('bai_file_name')
 tumour_bams = task_dict.get('input').get('tumour_bams')
@@ -26,29 +26,12 @@ study_id = task_dict.get('input').get('project_code')
 
 song_server = 'http://142.1.177.168:8080'
 
-subprocess.check_output(['docker', 'pull', payload_container])
 subprocess.check_output(['docker', 'pull', upload_container])
-
-subprocess.check_output(['docker','run','-v',input_directory+':/app',payload_container,'generate','-d',donor.get('id'),
-                         '-st',"DNA",'-at','sequencingRead','-l',experiment.get('library_strategy'),
-                         '-o','/app/payload.json',
-                         '--paired-end',
-                         '-ac', 'open',
-                         '-f','/app/'+normal_minibam_name,
-                         '/app/'+normal_minibai_name])
-
-#shutil.move(os.path.join(input_directory,'payload.json'), os.path.join(cwd,'payload.json'))
 
 subprocess.check_output(['docker','run','-e','ACCESSTOKEN','-v',input_directory+':/app',upload_container,'upload','-s',study_id,
                          '-u',song_server,'-p','payload.json','-o','/app/manifest.txt','-j','/app/manifest.json'])
 
 for tumour_bam in tumour_bams:
-    subprocess.check_output(['docker','run','-v',input_directory+':/app',payload_container,'generate_song_payload.py','-d',donor.get('id'),
-                             '-st',"DNA",'-at','sequencingRead','-l',experiment.get('library_strategy'),
-                             '-o','/app/payload.json',
-                             '--paired-end',
-                             '-f','/app/'+tumour_bam.get('minibam').get('bam_file_name'),
-                             '/app/'+tumour_bam.get('minibam').get('bam_file_name')])
 
 #shutil.move(os.path.join(input_directory,'payload.json'), os.path.join(cwd,'payload.json'))
 
