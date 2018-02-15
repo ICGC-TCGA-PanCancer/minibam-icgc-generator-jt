37b4ac2e064d02aa201f18f7a32eebb05fce2ec6
Brice Aminou <brice.aminou@gmail.com>
2018-02-15 14:36:07 -0500

    fix

--- workflow/tools/upload.py
+++ workflow/tools/upload.py
@@ -8,6 +8,18 @@ import sys
 
 import shutil
 
+def upload_file(input_directory, study_id, payload):
+    upload_container = "quay.io/baminou/dckr_song_upload"
+    song_server = 'http://142.1.177.168:8080'
+
+    subprocess.check_output(['docker', 'pull', upload_container])
+
+    subprocess.check_output(['docker','run','-e','ACCESSTOKEN',
+                             '-v', input_directory+':/app',upload_container, 'upload','-s',study_id,
+                             '-u', song_server, '-p', payload,
+                             '-o','manifest.txt','manifest.json',
+                             '-d', '/app/'])
+
 
 task_dict = get_task_dict(sys.argv[1])
 cwd = os.getcwd()
@@ -15,25 +27,12 @@ cwd = os.getcwd()
 save_output_json(task_dict)
 
 
-upload_container = "quay.io/baminou/dckr_song_upload"
-
 payloads = task_dict.get('input').get('payloads')
 input_directory = task_dict.get('input').get('input_directory')
-normal_minibam_name = task_dict.get('input').get('normal_bam').get('minibam').get('bam_file_name')
-normal_minibai_name = task_dict.get('input').get('normal_bam').get('minibam').get('bai_file_name')
-tumour_bams = task_dict.get('input').get('tumor_bams')
 study_id = task_dict.get('input').get('project_code')
 
-song_server = 'http://142.1.177.168:8080'
-
-subprocess.check_output(['docker', 'pull', upload_container])
-
 for i in range(0,len(payloads)):
-    subprocess.check_output(['docker','run','-e','ACCESSTOKEN','-v',input_directory+':/app',upload_container,'upload','-s',study_id,
-                         '-u',song_server,'-p',
-                             '/app/'+payloads[i],
-                             '-o','manifest.txt','-j','manifest.json',
-                             '-d',input_directory])
+    upload_file(input_directory, study_id, payloads[i])
 
 #save_output_json({
 #    'manifest_json': os.path.join(cwd,'payload.json')
