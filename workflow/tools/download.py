f542130986d6a71f6a4bfce66b462a448c3b5120
Brice Aminou <brice.aminou@gmail.com>
2018-02-15 15:43:34 -0500

    fix of download

--- workflow/tools/download.py
+++ workflow/tools/download.py
@@ -4,12 +4,9 @@ import subprocess
 import os
 from utils import get_task_dict, save_output_json
 import sys
-import time
 
 task_dict = get_task_dict(sys.argv[1])
 cwd = os.getcwd()
-
-#time.sleep(2*60)
 
 def download_file(object_id, out_dir, file_name):
     docker_container = "quay.io/baminou/minibam-collab-dckr:latest"
@@ -27,19 +24,19 @@ def download_file(object_id, out_dir, file_name):
 # Download normal bam file
 object_id = task_dict.get('input').get('normal_bam').get('object_id')
 file_name = task_dict.get('input').get('normal_bam').get('file_name')
-#download_file(object_id, cwd, file_name)
+download_file(object_id, cwd, file_name)
 
 # Download tumor bam files
 for i in range(0,len(task_dict.get('input').get('tumor_bams'))):
     object_id = task_dict.get('input').get('tumor_bams')[i].get('object_id')
     file_name = task_dict.get('input').get('tumor_bams')[i].get('file_name')
-    #download_file(object_id, cwd, file_name)
+    download_file(object_id, cwd, file_name)
 
 # Download VCF files
 for i in range(0,len(task_dict.get('input').get('vcf_files'))):
     object_id = task_dict.get('input').get('vcf_files')[i].get('object_id')
     file_name = task_dict.get('input').get('vcf_files')[i].get('file_name')
-    #download_file(object_id, cwd, file_name)
+    download_file(object_id, cwd, file_name)
 
 save_output_json({
     'directory': cwd
